2bfe216e1b7f4d0fc1eda95342d2a7b51207d91b
jaycammarano <jaycammarano@gmail.com>
2021-05-14 18:37:44 -0400

    created files and set up styles.css

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * A single page app theme for Azara Healthcare.
+ * Created as an assessment for Stirling Brandworks.
+ * 
+ *
+ * @package StirlingBrandworks
+ * @subpackage Azara_Healthcare
+ * 
+ */
+
+?>
